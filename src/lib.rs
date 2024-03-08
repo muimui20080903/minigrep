4e17f185ee17ca42e80e87318ea700e8dfa8edc4
muimui20080903 <98527235+muimui20080903@users.noreply.github.com>
2024-03-08 22:43:42 +0900

    complete chapter12

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,111 @@
-
+use std::env;
 use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 
-
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
             return Err("引数が足りません");
         }
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        let query: String = args[1].clone();
+        let filename: String = args[2].clone();
 
-        Ok(Config { query, filename })
+        let case_sensitive: bool = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
     }
 }
 
-pub fn run(config:Config)->Result<(),Box<dyn Error>>{
-    
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let mut f: File = File::open(config.filename)?;
 
     let mut contents = String::new();
     f.read_to_string(&mut contents)?;
 
-    println!("テキストは\n{}です", contents);
+    let results: Vec<&str> = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
 
-    Ok((   ))
+    for line in results {
+        println!("{}", line);
+    }
+
+    Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results: Vec<&str> = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query: String = query.to_lowercase();
+    let mut results: Vec<&str> = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        // Rust
+        // 安全かつ高速で生産的
+        // 三つを選んで
+        // ガムテープ
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        // Rust
+        // 安全かつ高速で生産的
+        // 三つを選んで
+        // 私を信じて
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,13 @@ use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("引数の解析中にエラーが発生しました:{}", err);
+    let config: Config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("引数の解析中にエラーが発生しました:{}", err);
         process::exit(1);
     });
 
-    if let Err(e) = minigrep::run(config){
-        println!("アプリケーションエラー:{}",e);
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("アプリケーションエラー:{}", e);
         process::exit(1);
     }
-
 }
