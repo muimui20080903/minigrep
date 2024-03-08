278f9a8fcd99b8f3b02e92546dac592d0f6d7e2d
muimui20080903 <98527235+muimui20080903@users.noreply.github.com>
2024-03-08 19:52:01 +0900

    complete 12.2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main(){
+    let args:Vec<String> = env::args().collect();
+    // println!("{:?}", args);
+
+    // let query:&String  = &args[1];
+    let filename:&String = &args[2];
+    // println!("{}を探しています", query);
+    // println!("{}というファイルの中から", filename);
+
+    let mut f:File = File::open(filename)
+    .expect("ファイルが見つかりませんでした");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+    .expect("ファイルの読み込みに失敗しました");
+
+    println!("テキストは\n{}です", contents);
 }
